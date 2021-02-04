ec8f03fdb2578578aaf6757361a820d5427e8d49
Jeandre van Zyl <Jeandre610@gmail.com>
2021-02-04 08:20:33 +0200

    inital Push
    Lets start this

---
+++ Barberco/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Barberco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
