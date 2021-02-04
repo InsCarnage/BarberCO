4b7345209cc88c74f937dc01e0ad3cae6397ce2c
AEKurten <78529029+AEKurten@users.noreply.github.com>
2021-02-04 12:37:04 +0200

    alot of changes

---
+++ Barberco/Areas/User/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Barberco.Models;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Barberco.Areas.User.Controllers
+{
+    [AllowAnonymous]
+    [Area("User")]
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Fetch all files in the Folder (Directory).
+            string[] filePaths = Directory.GetFiles("../Barberco/wwwroot/Images/");
+
+            //Copy File names to Model collection.
+            List<ProductModel> files = new List<ProductModel>();
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new ProductModel { FileName = Path.GetFileName(filePath) });
+            }
+
+            return View(files);
+        }
+    }
+}
---
+++ Barberco/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barberco.Models
+{
+    public class ProductModel
+    {
+        public string FileName {get; set;}
+    }
+}
